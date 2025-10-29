74366a44380159f3482b74b5b35639c2976d9f3d
Arman Patel <vanixgaming634@gmail.com>
2025-10-29 13:09:08 +0530

    feat: Update metadata and improve layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,14 @@ import Footer from "@/components/Footer";
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Arman Patel Portfolio Website",
+  title: "Arman Patel | Full Stack Developer (MERN, Next.js, Node.js)",
   description:
-    "This is my portfolio website to showcase my skills and projects",
-  icons:['/favicon/apple-touch-icon.png',"/favicon/favicon-32x32.png","/favicon/favicon-16x16.png"]
+    "Full Stack Developer skilled in React, Next.js, and Node.js. Explore my portfolio showcasing real-world web apps, projects, and experiences.",
+  icons: [
+    "/favicon/apple-touch-icon.png",
+    "/favicon/favicon-32x32.png",
+    "/favicon/favicon-16x16.png",
+  ],
 };
 
 export default function RootLayout({
@@ -21,26 +25,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-[#100f27]  text-AliceBlue scrollbar-hide">
-<head>
-<link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-</head>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={clsx(
           urbanist.className,
@@ -51,7 +55,7 @@ export default function RootLayout({
         {children}
         <div className="fixed inset-0 max-h-screen -z-50 background-gradient"></div>
         <div className="fixed bg-[url('/image/noise.jpg')] pointer-events-none inset-0 -z-40 h-full w-full   texture mix-blend-soft-light opacity-20 " />
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
